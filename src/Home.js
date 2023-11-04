c39b37be3805858272fe7c81ce1099252a50fb7f
nickwang <nick.wenkun.wang@outlook.com>
2023-11-04 17:59:02 +0800

    Issue: 注释import Updater: nickwang

--- src/Home.js
+++ src/Home.js
@@ -6,9 +6,9 @@ import NavbarView from "./modules/views/NavbarView";
 import ProductHero from "./modules/views/ProductHero";
 import ProductValues from "./modules/views/ProductValues";
 import ProductCategories from "./modules/views/ProductCategories";
-import ProductHowItWorks from "./modules/views/ProductHowItWorks";
-import ProductCTA from "./modules/views/ProductCTA";
-import ProductSmokingHero from "./modules/views/ProductSmokingHero";
+// import ProductHowItWorks from "./modules/views/ProductHowItWorks";
+// import ProductCTA from "./modules/views/ProductCTA";
+// import ProductSmokingHero from "./modules/views/ProductSmokingHero";
 import AppFooter from "./modules/views/AppFooter";
 
 const Home = () => (
